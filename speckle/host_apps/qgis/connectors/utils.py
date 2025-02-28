a6f450a390476c4306d2e8e3a49a0d1427a03e17
KatKatKateryna <kateryna@speckle.systems>
2025-02-28 12:28:14 +0000

    calculate and store metric vals in Utils

--- speckle/host_apps/qgis/connectors/utils.py
+++ speckle/host_apps/qgis/connectors/utils.py
@@ -1,32 +1,41 @@
+import os
 from typing import Callable
 from pathlib import Path
 from specklepy.logging import metrics
 
 from speckle.sdk.connectors_common.threading import ThreadContext
 
-from qgis.core import Qgis
+from qgis.core import Qgis, QgsApplication
 
 HOST_APP_FULL_VERSION = (
     Qgis.QGIS_VERSION.encode("iso-8859-1", errors="ignore")
     .decode("utf-8")
     .split("-")[0]
 )
-CORE_VERSION = "3.0.099"
-
 UNSUPPORTED_PROVIDERS = ["WFS", "wms", "wcs", "vectortile"]
 
 
 def get_core_version():
 
-    metadata_path = Path(Path(__file__).parent, "metadata.txt")
-    core_version = "3.0.099"
+    metadata_path = os.path.join(
+        QgsApplication.qgisSettingsDirPath(),
+        "python",
+        "plugins",
+        "speckle-qgis-v3",
+        "metadata.txt",
+    )
+    core_version = "3.0.099-alpha"
     with open(metadata_path, "r") as file:
         for i, line in enumerate(file.readlines()):
             if "version=" in line:
                 core_version = line.replace("version=", "").replace("\n", "")
+                break
     file.close()
 
     return core_version
+
+
+CORE_VERSION = get_core_version()
 
 
 def setup_metrics():
@@ -38,9 +47,6 @@ def setup_metrics():
         .split(".")[0]
     )
     metrics.set_host_app("qgis", version)
-
-    global CORE_VERSION
-    CORE_VERSION = get_core_version()
 
 
 class QgisThreadContext(ThreadContext):
